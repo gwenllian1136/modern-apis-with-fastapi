eb5eee2acb4955a4954b6d26039c625ad7ab1550
robtom <gwenllian1136@gmail.com>
2023-08-20 15:43:13 +0100

    json response

--- rob/main.py
+++ rob/main.py
@@ -11,9 +11,7 @@ def calculate(x :int, y : int , z : Optional[int] = None):
     value = x + y
 
     if z == 0 :
-        return fastapi.Response(content='{"error" : "ERROR : Z cannot be zero"}',
-                                media_type="application/json",
-                                status_code=400)
+        return fastapi.responses.JSONResponse(content={"error" : "ERROR : Z cannot be zero"}, status_code=400)
 
     if z is not None:
         value /= z
