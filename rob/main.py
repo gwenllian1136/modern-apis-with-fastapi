d6b320b674f287cdd9a534376438b04acdcdfda4
robtom <gwenllian1136@gmail.com>
2023-08-20 15:53:02 +0100

    added body of home page

--- rob/main.py
+++ rob/main.py
@@ -6,12 +6,27 @@ import uvicorn
 
 api=fastapi.FastAPI()
 
+@api.get('/')
+def index():
+    body = "<html>" \
+           "<body style='padding: 10px;'>" \
+           "<h1>Welcome to the API</h1>" \
+           "<div>" \
+           "Try it: <a href='/api/calculate?x=7&y=11'>/api/calculate?x=7&y=11</a>" \
+           "</div>" \
+           "</body>" \
+           "</html>"
+
+    return fastapi.responses.HTMLResponse(content=body)
+
+
 @api.get('/api/calculate')
 def calculate(x :int, y : int , z : Optional[int] = None):
     value = x + y
 
     if z == 0 :
-        return fastapi.responses.JSONResponse(content={"error" : "ERROR : Z cannot be zero"}, status_code=400)
+        return fastapi.responses.JSONResponse(content={"error" : "ERROR : Z cannot be zero"}
+                                              , status_code=400)
 
     if z is not None:
         value /= z
